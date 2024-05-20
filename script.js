c91b24f61a983afb23d16fcd6a7d527f1c235c2f
yusuf-wrhmn <yusufwarohman.gm@gmail.com>
2024-05-21 01:19:08 +0700

    Update script.js

--- script.js
+++ script.js
@@ -6,9 +6,7 @@ function JajarGenjang() {
 function LuasSegitiga() {
     let alas = document.getElementById("base").value
     let tinggi = document.getElementById("height").value
-
     let hasilluas = 0.5 * parseInt(alas) * parseInt(tinggi)
-
     document.getElementById("formula-area").innerHTML = "L"+" "+"="+" "+"1/2"+" "+"&times"+" "+"a"+" "+"&times"+" "+"t";
     document.getElementById("counting-area").innerHTML = "L"+" "+"="+" "+"1/2"+" "+"&times"+" "+alas+" "+"&times"+" "+ tinggi;
     document.getElementById("area").innerHTML = "L"+" "+"="+" "+hasilluas; 
@@ -26,9 +24,7 @@ function KelilingSegitiga() {
     let sisi1 = document.getElementById("side-a").value
     let sisi2 = document.getElementById("side-b").value
     let sisi3 = document.getElementById("side-c").value
-
     let hasilkeliling = parseInt(sisi1) + parseInt(sisi2) + parseInt(sisi3)
-
     document.getElementById("formula-circum").innerHTML = "K"+" "+"="+" "+"a"+" "+"+"+" "+"b"+" "+"+"+" "+"c";
     document.getElementById("counting-cricum").innerHTML = "K"+" "+"="+" "+sisi1+" "+"+"+" "+sisi2+" "+"+"+" "+sisi3;
     document.getElementById("circum").innerHTML = "K"+" "+"="+" "+hasilkeliling;
